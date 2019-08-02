c8abf0dac9264d8ccb5e4d3501866c714aeb73fd
s-h-a-n-k-s <ramsdentom@gmail.com>
2019-08-02 11:35:50 +0100

    Fix creating bookings

--- src/main/java/com/nationwide/booking/Booking.java
+++ src/main/java/com/nationwide/booking/Booking.java
@@ -1,6 +1,5 @@
 package com.nationwide.booking;
 
-import java.util.Arrays;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -13,7 +12,7 @@ public class Booking {
     private String firstName;
     private String lastName;
     private String showingId;
-    private Arrays seatIds;
+    private String[] seatIds;
     private double totalPrice;
     private boolean paid;
 
@@ -49,11 +48,11 @@ public class Booking {
         this.showingId = showingId;
     }
 
-    public Arrays getSeatIds() {
+    public String[] getSeatIds() {
         return seatIds;
     }
 
-    public void setSeatIds(Arrays seatIds) {
+    public void setSeatIds(String[] seatIds) {
         this.seatIds = seatIds;
     }
 
@@ -71,11 +70,12 @@ public class Booking {
 
     public void setPaid(boolean paid) {
         this.paid = paid;
-    }
+	}
+	
+	public Booking() {}
 
-    public Booking(String id, String firstName, String lastName, String showingId, Arrays seatIds, double totalPrice,
+    public Booking(String firstName, String lastName, String showingId, String[] seatIds, double totalPrice,
             boolean paid) {
-        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.showingId = showingId;
